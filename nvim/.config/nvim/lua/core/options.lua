ede5440bd7032f6aaf71efade5e4189e4dd2fb8a
Khalil Ben Khaled <khalil.benkhaled@esprit.tn>
2024-08-11 17:13:48 +0100

    batman

---
+++ nvim/.config/nvim/lua/core/options.lua
@@ -0,0 +1,45 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.g.have_nerd_font = false
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.mouse = "a"
+
+vim.opt.showmode = false
+
+vim.opt.clipboard = "unnamedplus"
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.signcolumn = "yes"
+
+-- Decrease update time
+vim.opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+vim.opt.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = "split"
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
+
---
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,34 @@
+return {
+  'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    -- Harpoon stuff, must change later
+    local harpoon = require 'harpoon'
+
+    -- REQUIRED
+    harpoon:setup()
+    -- REQUIRED
+
+    vim.keymap.set('n', '<leader>a', function()
+      harpoon:list():add()
+    end)
+    vim.keymap.set('n', '<leader>l', function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end)
+
+    vim.keymap.set('n', '<leader>&', function()
+      harpoon:list():select(1)
+    end)
+    vim.keymap.set('n', '<leader>é', function()
+      harpoon:list():select(2)
+    end)
+    vim.keymap.set('n', '<leader>"', function()
+      harpoon:list():select(3)
+    end)
+    vim.keymap.set('n', "<leader>'", function()
+      harpoon:list():select(4)
+    end)
+  end,
+}
+
